43390cf7e7302f0ab712918b30eb896ab4077cf6
sirius733 <sirius733@gmail.com>
2015-08-03 03:28:16 +0800

    groups finished

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,51 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only:[:show, :edit, :update, :destroy]
 	def index
-      flash[:notice] = "早安！你好！"
+    @groups = Group.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    # both OK 
+    # @group = Group.create(group_params)
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to @group, notice: 'Group was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @group.update(group_params)
+    if @group.save
+      redirect_to @group, notice: 'Group was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group.destroy
+    redirect_to groups_path, alert: 'Group was successfully destroyed.'
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:title, :description)
   end
 end
